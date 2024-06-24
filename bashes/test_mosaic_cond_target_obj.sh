7ab4f6e08daaa3e4f5cef30802eab322e3a2bfcd
ciccio42 <francescorosa97@virgilio.it>
2024-06-24 15:29:46 +0200

    add no mosaic method

--- bashes/test_mosaic_cond_target_obj.sh
+++ bashes/test_mosaic_cond_target_obj.sh
@@ -6,17 +6,17 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
 
 echo $1
 TASK_NAME="$1"
+NUM_WORKERS=7
+GPU_ID=1
 
 if [ "$TASK_NAME" == 'pick_place' ]; then
     BASE_PATH=/raid/home/frosa_Loc/Multi-Task-LFD-Framework
-    PROJECT_NAME=1Task-pick_place-Double-Policy-Contrastive-false-Inverse-false
+    PROJECT_NAME=1Task-pick_place-Double-Policy-Contrastive-false-Inverse-false-CONCAT_IMG_EMB-false-CONCAT_DEMO_EMB-true
     BATCH=32 #32
-    NUM_WORKERS=7
-    GPU_ID=0
     MODEL_PATH=/user/frosa/multi_task_lfd/checkpoint_save_folder/${PROJECT_NAME}-Batch${BATCH}
     CONTROLLER_PATH=$BASE_PATH/repo/Multi-Task-LFD-Training-Framework/tasks/multi_task_robosuite_env/controllers/config/osc_pose.json
     for MODEL in ${MODEL_PATH}; do
-        for S in 288630 262974; do #81000 89100; do
+        for S in 259767; do #81000 89100; do
             for TASK in pick_place; do
                 for COUNT in 1 2 3; do
                     if [ $COUNT -eq 1 ]; then
@@ -32,14 +32,12 @@ if [ "$TASK_NAME" == 'pick_place' ]; then
     done
 elif [ "$TASK_NAME" == 'nut_assembly' ]; then
     BASE_PATH=/raid/home/frosa_Loc/Multi-Task-LFD-Framework
-    PROJECT_NAME=1Task-nut_assembly-Double-Policy-Contrastive-false-Inverse-false-trial-2
+    PROJECT_NAME=1Task-nut_assembly-Double-Policy-Contrastive-false-Inverse-false-CONCAT_IMG_EMB-false-CONCAT_DEMO_EMB-true
     BATCH=27 #32
-    NUM_WORKERS=7
-    GPU_ID=0
-    MODEL_PATH=/user/frosa/multi_task_lfd/checkpoint_save_folder/${PROJECT_NAME}-Batch${BATCH}
+    MODEL_PATH=/user/frosa/multi_task_lfd/checkpoint_save_folder/from_cluster/checkpoint_save_folder/${PROJECT_NAME}-Batch${BATCH}
     CONTROLLER_PATH=$BASE_PATH/repo/Multi-Task-LFD-Training-Framework/tasks/multi_task_robosuite_env/controllers/config/osc_pose.json
     for MODEL in ${MODEL_PATH}; do
-        for S in 167760 165896; do #81000 89100; do
+        for S in 165896; do #81000 89100; do
             for TASK in nut_assembly; do
                 for COUNT in 1 2 3; do
                     if [ $COUNT -eq 1 ]; then
@@ -55,19 +53,17 @@ elif [ "$TASK_NAME" == 'nut_assembly' ]; then
     done
 elif [ "$TASK_NAME" == 'button' ]; then
     BASE_PATH=/raid/home/frosa_Loc/Multi-Task-LFD-Framework
-    PROJECT_NAME=1Task-press_button_close_after_reaching-Double-Policy-Contrastive-false-Inverse-false
+    PROJECT_NAME=1Task-press_button_close_after_reaching-Double-Policy-Contrastive-false-Inverse-false-CONCAT_IMG_EMB-false-CONCAT_DEMO_EMB-true
     BATCH=18 #32
-    NUM_WORKERS=4
-    GPU_ID=0
-    MODEL_PATH=/user/frosa/multi_task_lfd/checkpoint_save_folder/${PROJECT_NAME}-Batch${BATCH}
+    MODEL_PATH=/user/frosa/multi_task_lfd/checkpoint_save_folder/from_cluster/checkpoint_save_folder/${PROJECT_NAME}-Batch${BATCH}
     CONTROLLER_PATH=$BASE_PATH/repo/Multi-Task-LFD-Training-Framework/tasks/multi_task_robosuite_env/controllers/config/osc_pose.json
     for MODEL in ${MODEL_PATH}; do
-        for S in 97848; do #81000 89100; do
+        for S in 101472; do #81000 89100; do
             for TASK in button; do
-                for COUNT in 1; do
+                for COUNT in 1 2 3; do
                     if [ $COUNT -eq 1 ]; then
                         SAVE_PATH=${MODEL_PATH}/results_${TASK}/run_${COUNT}
-                        python $BASE_PATH/repo/Multi-Task-LFD-Training-Framework/test/multi_task_test/test_any_task.py $MODEL --env $TASK --saved_step $S --eval_each_task 10 --num_workers ${NUM_WORKERS} --project_name ${PROJECT_NAME} --controller_path ${CONTROLLER_PATH} --gpu_id ${GPU_ID} --save_path ${SAVE_PATH} --save_files
+                        python $BASE_PATH/repo/Multi-Task-LFD-Training-Framework/test/multi_task_test/test_any_task.py $MODEL --env $TASK --saved_step $S --eval_each_task 10 --num_workers ${NUM_WORKERS} --project_name ${PROJECT_NAME} --controller_path ${CONTROLLER_PATH} --gpu_id ${GPU_ID} --wandb_log --save_path ${SAVE_PATH} --save_files
                     else
                         SAVE_PATH=${MODEL_PATH}/results_${TASK}/run_${COUNT}
                         python $BASE_PATH/repo/Multi-Task-LFD-Training-Framework/test/multi_task_test/test_any_task.py $MODEL --env $TASK --saved_step $S --eval_each_task 10 --num_workers ${NUM_WORKERS} --project_name ${PROJECT_NAME} --controller_path ${CONTROLLER_PATH} --gpu_id ${GPU_ID} --wandb_log
@@ -79,19 +75,17 @@ elif [ "$TASK_NAME" == 'button' ]; then
 elif [ "$TASK_NAME" == 'stack_block' ]; then
     echo "STACK_BLOCK"
     BASE_PATH=/raid/home/frosa_Loc/Multi-Task-LFD-Framework
-    PROJECT_NAME=1Task-stack_block-Double-Policy-Contrastive-false-Inverse-false
+    PROJECT_NAME=1Task-stack_block-Double-Policy-Contrastive-false-Inverse-false-CONCAT_IMG_EMB-false-CONCAT_DEMO_EMB-true
     BATCH=18 #32
-    NUM_WORKERS=7
-    GPU_ID=0
-    MODEL_PATH=/user/frosa/multi_task_lfd/checkpoint_save_folder/${PROJECT_NAME}-Batch${BATCH}
+    MODEL_PATH=/user/frosa/multi_task_lfd/checkpoint_save_folder/from_cluster/checkpoint_save_folder/${PROJECT_NAME}-Batch${BATCH}
     CONTROLLER_PATH=$BASE_PATH/repo/Multi-Task-LFD-Training-Framework/tasks/multi_task_robosuite_env/controllers/config/osc_pose.json
     for MODEL in ${MODEL_PATH}; do
-        for S in 119196 105952 75680; do
+        for S in 157036; do
             for TASK in stack_block; do
                 for COUNT in 1 2 3; do
                     if [ $COUNT -eq 1 ]; then
                         SAVE_PATH=${MODEL_PATH}/results_${TASK}/run_${COUNT}
-                        python $BASE_PATH/repo/Multi-Task-LFD-Training-Framework/test/multi_task_test/test_any_task.py $MODEL --env $TASK --saved_step $S --eval_each_task 10 --num_workers ${NUM_WORKERS} --project_name ${PROJECT_NAME} --controller_path ${CONTROLLER_PATH} --gpu_id ${GPU_ID} --wandb_log #--save_path ${SAVE_PATH} --save_files
+                        python $BASE_PATH/repo/Multi-Task-LFD-Training-Framework/test/multi_task_test/test_any_task.py $MODEL --env $TASK --saved_step $S --eval_each_task 10 --num_workers ${NUM_WORKERS} --project_name ${PROJECT_NAME} --controller_path ${CONTROLLER_PATH} --gpu_id ${GPU_ID} --wandb_log --save_path ${SAVE_PATH} --save_files
                     else
                         SAVE_PATH=${MODEL_PATH}/results_${TASK}/run_${COUNT}
                         python $BASE_PATH/repo/Multi-Task-LFD-Training-Framework/test/multi_task_test/test_any_task.py $MODEL --env $TASK --saved_step $S --eval_each_task 10 --num_workers ${NUM_WORKERS} --project_name ${PROJECT_NAME} --controller_path ${CONTROLLER_PATH} --gpu_id ${GPU_ID} --wandb_log
@@ -105,7 +99,7 @@ elif [ "$TASK_NAME" == 'multi' ]; then
     BASE_PATH=/raid/home/frosa_Loc/Multi-Task-LFD-Framework
     PROJECT_NAME=1Task-Multi-Task-Double-Policy-Contrastive-false-Inverse-false
     BATCH=74 #32
-    NUM_WORKERS=1
+    NUM_WORKERS=7
     GPU_ID=3
     MODEL_PATH=/user/frosa/multi_task_lfd/checkpoint_save_folder/${PROJECT_NAME}-Batch${BATCH}
     CONTROLLER_PATH=$BASE_PATH/repo/Multi-Task-LFD-Training-Framework/tasks/multi_task_robosuite_env/controllers/config/osc_pose.json
---
+++ bashes/test_mosaic_cond_target_obj_run_multiple_test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source test_mosaic_cond_target_obj.sh nut_assembly
+
+source test_mosaic_cond_target_obj.sh stack_block
+
+source test_mosaic_cond_target_obj.sh button
